8b6abeb0f2a3688c5cd625d6db6e19a1ee672055
SergeyRazumenko <sergeyrazumenko85@gmail.com>
2023-09-15 19:22:27 +0300

    Cream ready

--- src/static/js/tovarka-scripts.js
+++ src/static/js/tovarka-scripts.js
@@ -3,51 +3,98 @@
 document.addEventListener( 'DOMContentLoaded', function () {
 
     /* Characteristics slider init */
+    const characterSlider = document.getElementById("character-slider");
+    const characterThumbSlider = document.getElementById("character-thumb-slider");
 
-    const main = new Splide( '#character-slider', {
-        type      : 'fade',
-        rewind    : true,
-        pagination: false,
-        arrows    : false,
-    } );
+    const initCharacterSlider = function(slider, sliderThumbs) {
+        const main = new Splide( slider, {
+            type      : 'fade',
+            rewind    : true,
+            pagination: false,
+            arrows    : false,
+        } )
+        const thumbnails = new Splide( sliderThumbs, {
+            fixedWidth  : 94,
+            fixedHeight : 94,
+            gap         : 4,
+            rewind      : true,
+            pagination  : false,
+            cover       : true,
+            isNavigation: true,
+        } )
 
-    const thumbnails = new Splide( '#character-thumb-slider', {
-        fixedWidth  : 94,
-        fixedHeight : 94,
-        gap         : 4,
-        rewind      : true,
-        pagination  : false,
-        cover       : true,
-        isNavigation: true,
-    } );
+        main.sync( thumbnails );
+        main.mount();
+        thumbnails.mount();
+    }
 
-    main.sync( thumbnails );
-    main.mount();
-    thumbnails.mount();
+    characterSlider && characterThumbSlider ? initCharacterSlider(characterSlider, characterThumbSlider) : '';
+ 
 
     /* Reviews slider init */
 
-    const reviews = new Splide( '#reviews-slider', {
-        type      : 'loop',
-        rewind    : true,
-        fixedHeight: true,
-        pagination: false,
-        arrows    : false,
-    } );
+    const reviewsSlider = document.getElementById("reviews-slider");
+    const reviewsSliderAvatars = document.getElementById("reviews-slider-avatars");
+    
+    const initReviewsSlider = function(slider, sliderThumbs) {
+        const reviews = new Splide( slider, {
+            type      : 'loop',
+            rewind    : true,
+            fixedHeight: true,
+            pagination: false,
+            arrows    : false,
+        } );
+    
+        const avatars = new Splide( sliderThumbs, {
+            fixedWidth  : 50,
+            fixedHeight : 50,
+            gap         : 16,
+            rewind      : true,
+            pagination  : false,
+            cover       : true,
+            isNavigation: true,
+        } );
+    
+        reviews.sync( avatars );
+        reviews.mount();
+        avatars.mount();
+    };
 
-    const avatars = new Splide( '#reviews-slider-avatars', {
-        fixedWidth  : 50,
-        fixedHeight : 50,
-        gap         : 16,
-        rewind      : true,
-        pagination  : false,
-        cover       : true,
-        isNavigation: true,
-    } );
+    reviewsSlider && reviewsSliderAvatars ? initReviewsSlider(reviewsSlider, reviewsSliderAvatars) : '';
 
-    reviews.sync( avatars );
-    reviews.mount();
-    avatars.mount();
+
+    /* Cream Characteristics slider init */
+
+    const creamCharacterSlider = document.getElementById("cream-character-slider");
+    const initCreamCharacterSlider = function(slider) {
+
+        var creamCharacter = new Splide( slider, {
+            type   : 'loop',
+            padding: '20px',
+            arrows    : false,
+          } );
+          
+          creamCharacter.mount();
+    }
+
+    creamCharacterSlider ? initCreamCharacterSlider(creamCharacterSlider) : '';
+
+    /* Cream Review slider init */
+
+    const creamReviewsSlider = document.getElementById("cream-reviews-slider");
+    const initCreamReviewsSlider = function(slider) {
+
+        var creamCharacter = new Splide( slider, {
+            type      : 'loop',
+            rewind    : true,
+            fixedHeight: true,
+            arrows    : false,
+          } );
+          
+          creamCharacter.mount();
+    }
+
+    creamReviewsSlider ? initCreamReviewsSlider(creamReviewsSlider) : '';
 
     const tabs = new Tabby('[data-tabs]');
 
